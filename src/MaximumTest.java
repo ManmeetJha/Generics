4a0911542d0dd728e4448b21b071eefa084c0aca
Navneet <navneetjh5@gmail.com>
2020-10-16 17:07:37 +0530

    Generic with three variables

--- src/MaximumTest.java
+++ src/MaximumTest.java
@@ -1,87 +1,30 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static java.lang.StrictMath.E;
 import static org.junit.Assert.assertEquals;
 
 public class MaximumTest {
 
     @Test
-    public void maximumValueIntegerTC() {
-        int max;
-
-        //Input for TC1.1
-        Integer[] input1 = {6, 2, 3};
-        max = Maximum.getMaximumInteger(input1);
-        Assert.assertEquals(6, max);
-
-        //Input for TC1.2
-        Integer[] input2 = {2, 6, 3};
-        max = Maximum.getMaximumInteger(input2);
-        Assert.assertEquals(6, max);
-
-        //Input for TC1.3
-        Integer[] input3 = {1, 4, 6};
-        max = Maximum.getMaximumInteger(input3);
-        Assert.assertEquals(6, max);
-    }
-
-    @Test
-    public void maximumValueFloatTC() {
-        float max;
-
-        //Input for TC2.1
-        Float[] input1 = {6.0f, 2.0f, 3.0f};
-        max =Maximum.getMaximumFloat(input1);
-        Assert.assertEquals(6.0f, max,0.0f);
-
-        //Input for TC2.2
-        Float[] input2 = {2.0f, 6.0f, 3.0f};
-        max =Maximum.getMaximumFloat(input2);
-        Assert.assertEquals(6.0f, max, 0.0f);
-
-        //Input for TC2.3
-        Float[] input3 = {1.0f, 4.0f, 6.0f};
-        max =Maximum.getMaximumFloat(input3);
-        Assert.assertEquals(6.0f, max,0.0f);
-    }
-
-    @Test
-    public void maximumValueStringTC() {
-        String max;
-
-        //Input for TC3.1
-        String[] input1 = {"Peach" ,"Apple", "Banana"};
-        max =Maximum.getMaximumString(input1);
-        Assert.assertEquals("Peach",max);
-
-        //Input for TC3.2
-        String[] input2 = {"Apple", "Peach" ,"Banana"};
-        max =Maximum.getMaximumString(input2);
-        Assert.assertEquals("Peach",max);
-
-        //Input for TC3.3
-        String[] input3 = {"Apple","Banana", "Peach" };
-        max =Maximum.getMaximumString(input3);
-        Assert.assertEquals("Peach",max);
-   }
-
-    @Test
-    public <E> void maximumValueGenericTC() {
+    public <E> void testMaximumTEST()
+    {
         E max;
 
-        //Generic_Integer
-        Integer[] input1 = {6,2,3};
-        max = (E) Maximum.getMaximumGeneric(input1);
-        Assert.assertEquals(6,max);
+        //Integer
+        Maximum input1=new Maximum(10,20,30);
+        max= (E)input1.testMaximum();
+        Assert.assertEquals(30,max);
 
-        //Generic_Float
-        Float[] input2 = {6.0f, 2.0f, 3.0f};
-        max = (E) Maximum.getMaximumGeneric(input2);
-        Assert.assertEquals(6.0f,max);
+        //Float
+        Maximum input2=new Maximum(10.0,20.0,30.0);
+        max= (E)input2.testMaximum();
+        Assert.assertEquals(30.0,max);
 
-        //Generic_String
-        String[] input3 = {"Apple","Banana", "Peach" };
-        max = (E) Maximum.getMaximumGeneric(input3);
+        //String
+        Maximum input3=new Maximum("Apple","Banana","Peach");
+        max= (E)input3.testMaximum();
         Assert.assertEquals("Peach",max);
+
     }
 }
